543581a0875de6e3b7f8e25c37bc421b3b1051b6
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-10 16:17:31 +0300

    Removed extra wrappers

--- bots/quiz.py
+++ bots/quiz.py
@@ -1,44 +1,32 @@
-import os
-import sys
 import random
 import re
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-QUESTIONS_FOLDER = "quiz-questions/"
 
-
-def parse_questions_and_answers() -> tuple:
-    quiz_filenames = os.listdir(QUESTIONS_FOLDER)
-    questions = []
-    answers = []
-
-    for filename in quiz_filenames:
-        filepath = os.path.join(QUESTIONS_FOLDER, filename)
-
-        with open(filepath, "r", encoding="KOI8-R") as file:
-            quiz_file = file.read().split("\n\n")
-
-        for row in quiz_file:
-            if re.search("Вопрос", row):
-                question_text = row.split("\n")[1:]
-                question = " ".join(question_text)
-
-                if not question.find("Вопрос"):
-                    start_index = question.find(":") + 2
-                    question = question[start_index:]
-
-                questions.append(question)
-
-            if re.search("Ответ", row):
-                answer_text = row.split("\n")[1:]
-                answer = " ".join(answer_text)
-                answers.append(answer)
-
-    return questions, answers
+QUIZ_FILEPATH = "quiz-questions/quizzes.txt"
 
 
 def get_questions_and_answers() -> list:
-    questions, answers = parse_questions_and_answers()
+    questions = []
+    answers = []
+
+    with open(QUIZ_FILEPATH, "r", encoding="KOI8-R") as file:
+        quiz_file = file.read().split("\n\n")
+
+    for row in quiz_file:
+        if re.search("Вопрос", row):
+            question_text = row.split("\n")[1:]
+            question = " ".join(question_text)
+
+            if not question.find("Вопрос"):
+                start_index = question.find(":") + 2
+                question = question[start_index:]
+            questions.append(question)
+
+        if re.search("Ответ", row):
+            answer_text = row.split("\n")[1:]
+            answer = " ".join(answer_text)
+            answers.append(answer)
+
     quizzes = dict(zip(questions, answers))
     questions_and_answers = []
 
@@ -52,9 +40,7 @@ def get_questions_and_answers() -> list:
     return questions_and_answers
 
 
-def get_random_question() -> str:
-    loaded_quiz = get_questions_and_answers()
+def get_random_question(loaded_quiz: list) -> str:
     questions_count = len(loaded_quiz) - 1
     random_question_index = random.randint(0, questions_count)
-
     return loaded_quiz[random_question_index]["Вопрос"]
